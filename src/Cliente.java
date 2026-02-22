d4fb83d74e6f6c27ff9e22e5a81c4ff048c4ca96
Javier <javier@MacBook-Pro-de-Javier.local>
2026-02-22 03:40:59 -0500

    Se agrega clase Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente {
+
+    private String nombre;
+    private String documento;
+
+    public Cliente(String nombre, String documento) {
+
+        this.nombre = nombre;
+        this.documento = documento;
+
+    }
+
+    public String getNombre() {
+
+        return nombre;
+
+    }
+
+}
