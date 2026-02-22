047beae056dcb21fa7e20666b890a467df77801a
Javier <javier@MacBook-Pro-de-Javier.local>
2026-02-22 03:39:18 -0500

    Se agrega clase Producto

---
+++ src/Producto.java
@@ -0,0 +1,21 @@
+public class Producto {
+
+    private String nombre;
+    private double precio;
+    private int cantidad;
+
+    public Producto(String nombre, double precio, int cantidad) {
+
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+
+    }
+
+    public double calcularTotal() {
+
+        return precio * cantidad;
+
+    }
+
+}
